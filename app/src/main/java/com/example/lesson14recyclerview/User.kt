7cdfaaea39193ac1e293e24ecdf20062bf819d97
sharap1992 <sharapsagidullaev@gmail.com>
2020-09-24 18:10:47 +0500

    added additional func

--- app/src/main/java/com/example/lesson14recyclerview/User.kt
+++ app/src/main/java/com/example/lesson14recyclerview/User.kt
@@ -1,8 +1,6 @@
 package com.example.lesson14recyclerview
 
 class User() {
-    var title: String = ""
-    var description: String = ""
     var name1: String = ""
     var name2: String = ""
     var count: Int = 1
